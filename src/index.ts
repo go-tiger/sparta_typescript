64f1cf0e23aa3df429d6633915f8b22242b3a76a
go-tiger <inter_tiger_@naver.com>
2023-01-28 00:27:48 +0900

    utility types / required

--- src/index.ts
+++ src/index.ts
@@ -17,4 +17,21 @@ const partialToppingsIWant: Partial<Toppings> = {
   onion: undefined,
 };
 
-console.log(partialToppingsIWant);
+// console.log(partialToppingsIWant);
+
+/* 
+서드파티 라이브러리에서 타입을 가져와서 쓰는 경우가 많다
+그 라이브러리에서 export 해주는 타입만 쓸수있는데
+그 타입에 속해있는 다른 타입들은 export가 되지않을 수 있다.
+지정되어있는 타입들을 유용하게 쓸수있도록 사용하기 위해
+*/
+
+interface BubbleTeaOrder {
+  tea: boolean;
+  straw?: boolean;
+}
+
+const myBubbleTeaOrder: Required<BubbleTeaOrder> = {
+  tea: true,
+  straw: true,
+};
