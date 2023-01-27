47e42b2e0369dae45b6415efbfb66429ddf6cf77
go-tiger <inter_tiger_@naver.com>
2023-01-27 15:54:34 +0900

    Enum 객체 구조 분해할당

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-const enum Color {
-  Red = 'Red',
-  Green = 'Green',
-  Blue = 'Blue',
-}
+import { Color } from './types';
 
-const green = Color.Green;
+const { Red, Green, Blue: ImBlue } = Color;
+
+const green = Green;
+
+const veggies = green;
+
+const pepper = Red;
+
+const sky = ImBlue;
+
+console.log(sky);
