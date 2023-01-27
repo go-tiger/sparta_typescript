f5f800d10bc3bc43b8be1f05413ba0beb9aa4c0d
go-tiger <inter_tiger_@naver.com>
2023-01-27 16:03:16 +0900

    enumTips 객체 구조 분해할당

---
+++ src/others/enumTips.ts
@@ -0,0 +1,13 @@
+import { Color } from '../types';
+
+const { Red, Green, Blue: ImBlue } = Color;
+
+const green = Green;
+
+const veggies = green;
+
+const pepper = Red;
+
+const sky = ImBlue;
+
+console.log(sky);
