999122fde8424f0d435b404441f26f03c9974ffb
go-tiger <inter_tiger_@naver.com>
2023-01-27 15:47:46 +0900

    사용하지 않는것도 트랜스파일

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Color;
+(function (Color) {
+    Color["Red"] = "Red";
+    Color["Green"] = "Green";
+    Color["Blue"] = "Blue";
+})(Color || (Color = {}));
